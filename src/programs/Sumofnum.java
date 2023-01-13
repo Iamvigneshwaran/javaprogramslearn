84e06fb5a1b60b5464e2dedd5463b1c16ed12be0
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-13 23:55:16 +0530

    add num util negative num

--- src/programs/Sumofnum.java
+++ src/programs/Sumofnum.java
@@ -1,5 +1,44 @@
 package programs;
 
-public class Sumofnum {
 
-}
+	import java.util.Scanner;
+	public class Sumofnum {
+
+
+		Scanner sc=new Scanner(System.in);
+
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+//			Scanner sc=new Scanner(System.in);
+			
+			Sumofnum sn=new Sumofnum();
+			int n;
+	        System.out.println("Enter the number if u stop the loop enter the negative number");
+			
+			
+			int su=sn.sumofnum();
+			System.out.println("sum="+su);
+		}
+
+		
+		
+		public int sumofnum(){
+			
+			int n,sum=0;
+			
+			
+			while(true){
+				n=sc.nextInt();
+				if(n<0){
+				 break;
+				}
+				sum=sum+n;
+			}
+			return sum;
+		}
+		
+
+	}
+
